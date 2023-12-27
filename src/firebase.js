dba60a6a7c0c159b94bd4c092baf868970701878
Ashutoshbot1 <singhashutoshsa2126@gmail.com>
2023-12-27 15:57:30 +0530

    setting up env file

--- src/firebase.js
+++ src/firebase.js
@@ -4,27 +4,26 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCobocmnLUw22q2VwfF55WdqHi-u2ESouk",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "podcast-react-2.firebaseapp.com",
   projectId: "podcast-react-2",
   storageBucket: "podcast-react-2.appspot.com",
   messagingSenderId: "732621459594",
   appId: "1:732621459594:web:5f18cbe185a118dd99dd73",
-  measurementId: "G-2Q1BBM0EX6"
+  measurementId: "G-2Q1BBM0EX6",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const db=getFirestore(app);
-const storage=getStorage(app);
-const auth=getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
 
-export {auth,db,storage};
+export { auth, db, storage };
